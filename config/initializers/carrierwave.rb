47795604c03416111b9f6dfd874311aa5173552f
Soya Kasuga <edogawa.0255@gmail.com>
2020-05-20 17:50:43 +0900

    アップローダー系設定の修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,19 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: 'AKIA2MWQQKK2D7DXQBSD',
-      aws_secret_access_key: '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',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region: 'ap-northeast-1'
     }
-
+    config.fog_provider = 'fog/aws'
     config.fog_directory  = 'www.memoryfootprint.com'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/www.memoryfootprint.com'
     config.cache_storage = :fog
   end
 end
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
