b990595181b3885d2c9ee22c740648c5f29b000d
Soya Kasuga <edogawa.0255@gmail.com>
2020-06-04 17:20:51 +0900

    フォロー機能の構築(完成)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update,:destroy]
+  before_action :logged_in_user, only: [:edit, :update,:destroy,:following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
   
@@ -10,9 +10,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
-    @microposts_map = @user.microposts
-    @lat = @user.microposts.pluck(:latitude)
-    @lng = @user.microposts.pluck(:longitude)
   end
   
   def create
@@ -49,6 +46,18 @@ class UsersController < ApplicationController
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
     redirect_to users_url
+  end
+  
+  def following
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 
   private
