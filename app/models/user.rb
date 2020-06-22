040d9c92b13b7ca82b7373b98cbf13f9a1b99a8c
Soya Kasuga <edogawa.0255@gmail.com>
2020-06-22 20:54:32 +0900

    レイアウトの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   has_many :likes, dependent:  :destroy
+  has_many :like_microposts, through: :likes, source: :micropost
   attr_accessor :remember_token
   mount_uploader :picture, PictureUploader
   before_save { email.downcase! }
