97bccdf7520bce5177f1e903e9ba96ada45e19cc
Soya Kasuga <edogawa.0255@gmail.com>
2020-06-25 20:14:02 +0900

    テストユーザーの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update,:destroy,:following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
+
   
   def new
     @user = User.new
@@ -33,12 +34,13 @@ class UsersController < ApplicationController
   
   def edit
     @user = User.find(params[:id])
+    testuser = User.find_by(email: "testuser@test.com")
   end
   
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      flash[:success] = "Profile updated"
+      flash[:success] = "プロフィールを更新しました"
       redirect_to @user
     else
       render 'edit'
@@ -51,7 +53,7 @@ class UsersController < ApplicationController
   
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
+    flash[:success] = "ユーザーを消去しました"
     redirect_to users_url
   end
   
@@ -75,8 +77,6 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation,:introduction,:picture)
     end
-    
-    
     
     def correct_user
       @user = User.find(params[:id])
