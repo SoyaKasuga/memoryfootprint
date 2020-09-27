4f3a9b3ff093f850884628b835cdc553edfd9779
Soya Kasuga <edogawa.0255@gmail.com>
2020-09-27 20:00:11 +0900

    test:micropostのrequest specを追加

--- spec/requests/microposts_spec.rb
+++ spec/requests/microposts_spec.rb
@@ -3,6 +3,43 @@ require 'rails_helper'
 RSpec.describe 'Micropost', type: :request do
   let!(:user) { create(:user) }
 
+  describe 'GET #new' do
+    it 'renders a successful response' do
+      log_in_as(user)
+      get new_micropost_path
+      expect(response).to have_http_status 200
+    end
+  end
+
+  describe 'GET #index' do
+    it 'renders a successful response' do
+      get microposts_path
+      expect(response).to have_http_status 200
+    end
+  end
+
+  describe 'GET #show' do
+    let(:micropost) { create(:micropost) }
+    it 'renders a successful response' do
+      get micropost_path(micropost)
+      expect(response).to have_http_status 200
+    end
+  end
+
+  describe 'GET #rank' do
+    it 'renders a successful response' do
+      get microposts_rank_path
+      expect(response).to have_http_status 200
+    end
+  end
+
+  describe 'GET #search' do
+    it 'renders a successful response' do
+      get microposts_search_path
+      expect(response).to have_http_status 200
+    end
+  end
+
   describe 'POST #create' do
     it 'create a micropost and redirect to user page' do
       log_in_as(user)
