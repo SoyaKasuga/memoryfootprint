00404c3fea8619665fbb9e2d48afb158324fa722
Soya Kasuga <edogawa.0255@gmail.com>
2020-10-07 10:04:50 +0900

    地図のマーカーアイコンの変更

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -4,6 +4,11 @@ class MapsController < ApplicationController
     @hash = Gmaps4rails.build_markers(@microposts) do |micropost, marker|
       marker.lat micropost.latitude
       marker.lng micropost.longitude
+      marker.picture({
+        :url => "https://memoryfootbucket.s3-ap-northeast-1.amazonaws.com/uploads/direct-access/picture/48pxfootprint.png",
+        :width   => 48,
+        :height  => 48
+      })
       marker.infowindow render_to_string(partial: 'maps/infowindow', locals: { micropost: micropost })
     end
   end
